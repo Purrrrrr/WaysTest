fa2c18cb47d948ffe684090d4be0825a9064df7e
David Consuegra <david@>
2014-07-07 12:34:34 +0300

    Added UI-router and some animations

--- js/theways.js
+++ js/theways.js
@@ -1,5 +1,38 @@
-var app = angular.module('theways', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.typeahead', 'ui.bootstrap.datepicker']);
-
+var theWaysApp = angular.module('theways', ['ngAnimate', 'ui.router']);
+theWaysApp.config(function($stateProvider, $urlRouterProvider) {
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/home");
+  // Now set up the states
+  $stateProvider
+  .state('home', {
+    url: "/home",
+    templateUrl: "partials/home.html"
+  })
+  .state('ways', {
+    url: "/ways",
+    templateUrl: "partials/ways.html"
+  })
+  .state('ways.way', {
+    url: "/way",
+    templateUrl: "partials/way.html",
+    controller: function($scope) {
+      $scope.items = ["A", "List", "Of", "Items"];
+    }
+    }) 
+   /*
+  .state('state2', {
+    url: "/state2",
+    templateUrl: "partials/state2.html"
+  })
+  .state('state2.list', {
+    url: "/list",
+    templateUrl: "partials/state2.list.html",
+    controller: function($scope) {
+      $scope.things = ["A", "Set", "Of", "Things"];
+    }
+  })
+  */
+});
 /*
 app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
         $routeProvider.when('/', {

45ec980ab641157a1d46dca4ffe2336286da9204
David Consuegra <david@>
2014-07-13 19:52:10 +0300

    Working animations and base view system

--- js/theways.js
+++ js/theways.js
@@ -8,102 +8,58 @@ theWaysApp.config(function($stateProvider, $urlRouterProvider) {
     url: "/splash",
     templateUrl: "partials/splash.html"
   })
-  .state('stack', {
+  .state('ways', {
     abstract: true,
     templateUrl: "partials/layout.html"
   })
-  .state('stack.ba', {
-    url: "/ba",
-    data: {depth: 1},
-    template: "<h1>BA</h1><ui-view ng-class='animationClasses' class='toggle'/>"
-  })
-  .state('stack.aa', {
-    url: "/aa",
-    data: {depth: 2},
-    template: "<h1>AA</h1><ui-view ng-class='animationClasses' class='toggle'/>"
-  })
-  .state('stack.aa.bb', {
-    url: "/bb",
-    template: "<h1>BB</h1><ui-view ng-class='animationClasses' class='toggle'/>"
-  })
-  .state('stack.aa.bb.cc', {
-    url: "/cc",
-    template: "<h1>CC</h1><ui-view ng-class='animationClasses' class='toggle'/>"
-  })
-  .state('stack.aa.bb.bb', {
-    url: "/bb",
-    template: "<h1>BB</h1><ui-view/>"
-  }) .state('stack.home', {
-    url: "/home",
-    templateUrl: "partials/home.html"
-  })
-  .state('results', {
-    url: "/results",
-    templateUrl: "partials/results.html",
-    }) 
-  .state('way', {
-    url: "/way",
-    templateUrl: "partials/results.way.html",
-    }) 
-  .state('results.way', {
-    url: "/way",
-    templateUrl: "partials/results.way.html",
-    }) 
-   /*
-  .state('state2', {
-    url: "/state2",
-    templateUrl: "partials/state2.html"
-  })
-  .state('state2.list', {
-    url: "/list",
-    templateUrl: "partials/state2.list.html",
-    controller: function($scope) {
-      $scope.things = ["A", "Set", "Of", "Things"];
+  .state('ways.map', {
+    url: "/ways/map",
+    data: {position: "left"},
+    views: {
+      "map": {
+        templateUrl: "partials/ways.map.html",
+      }
     }
   })
-  */
-}).run(['$rootScope', '$state', function ($rootScope, $state) {
+  .state('ways.list', {
+    url: "/ways/list",
+    data: {position: "right"},
+    views: {
+      "list": {
+        templateUrl: "partials/ways.list.html",
+      }
+    }
+  })
+  .state('ways.details', {
+    url: "/ways/way",
+    data: {position: "bottom"},
+    views: {
+      "details": {
+        templateUrl: "partials/ways.details.html",
+      }
+    }
+  })
+})
+/* Animation class magik */
+.run(['$rootScope', '$state', function ($rootScope, $state) {
   
-  $rootScope.animationClasses = ['toggle']
+  $rootScope.viewClasses = []
   $rootScope.$on("$stateChangeStart", function (event, toState, toParams, fromState, fromParams) {
-    var depth = function(state) {
-      return state.name.split(".").length;
-    };
-    var fromDepth = fromState.data ? fromState.data.depth : 0;
-    var toDepth = toState.data ? toState.data.depth : 0;
-
-    if (fromDepth > toDepth) {
-      $rootScope.animationClasses = ["sliding", "sliding-from-right"]
-    } else {
-      $rootScope.animationClasses = ['toggle',    ]
+    var viewClasses = [];
+    if (fromState.data && fromState.data.position) {
+      viewClasses.push("from-"+fromState.data.position);
     }
-    /**/
-
-     console.log(fromDepth+"->"+toDepth);
-     console.log(toState);
-     console.log(toParams);
+    if (toState.data && toState.data.position) {
+      viewClasses.push("to-"+toState.data.position);
+    }
+    $rootScope.viewClasses = viewClasses;
  });
+}])
+.directive("uiView", function() {
+  return {
+    link: function(scope, elem, attrs) {
+      elem.toggleClass("empty-view", elem.children().length == 0);
+    }
+  };
+});;
 
-}]);
-
-/*
-app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
-        $routeProvider.when('/', {
-            controller: 'MainController',
-            templateUrl: 'partials/main.html'
-        }).when('/students', {
-            controller: 'StudentsController',
-            templateUrl: 'partials/students.html'
-        }).when('/students/:studentNumber', {
-            controller: 'StudentController',
-            templateUrl: 'partials/student.html'
-        }).otherwise({redirectTo: '/'});
-        
-        $httpProvider.defaults.withCredentials = true;
-
-      }]);
-
-app.controller("MainController", function($scope, $filter) {
-
-});
-*/

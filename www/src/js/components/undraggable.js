4add5499be67da1a83a31a8e07cc64b6b9639275
David Consuegra <david@>
2014-09-15 17:22:52 +0300

    Fixed undraggable

--- www/src/js/components/undraggable.js
+++ www/src/js/components/undraggable.js
@@ -1,12 +1,12 @@
 /** Implements the grayscale coloring of the ways map */
 theWaysApp.directive('undraggable', 
-[function() {
+['$ionicSlideBoxDelegate', function($ionicSlideBoxDelegate) {
   return {       
     restrict: 'A', 
     priority: 100,
     link: function postLink(originalScope, iElement, iAttrs, mapController) {
       //var events = ['mousedown', 'touchstart', 'touchmove', 'mousemove', 'touchend', 'mouseup'];
-      var events = 'touchmove, mousemove';
+      var events = 'touchstart mousedown';
       iElement.on(events, function(e) {
         e.stopPropagation();
       });
